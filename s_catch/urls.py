b2727110bb0c9756366b9cf51f61d84547eda246
chumakov <ilya.chumakov99@gmail.com>
2022-03-21 13:41:41 +0300

    remove admin sidebar

--- s_catch/urls.py
+++ s_catch/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('endpoints.urls')),
